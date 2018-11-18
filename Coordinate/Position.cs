2f5f8e64f3165bb6a633f4901cbb69f64ace098b
Glutch <oliverwiljohansson@gmail.com>
2018-11-18 16:18:48 +0100

    first commit

---
+++ Coordinate/Position.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coordinate
+{
+    class Position
+    {
+        private double x;
+        private double y;
+
+        public double X
+        {
+            get => this.x;
+            set => this.x = Math.Max(value, 0);
+        }
+
+        public double Y
+        {
+            get => this.y;
+            set => this.y = Math.Max(value, 0);
+        }
+
+        public Position(double inputX, double inputY)
+        {
+            X = inputX;
+            Y = inputY;
+        }
+
+        public double Length() =>
+            Math.Sqrt(Math.Pow(this.X, 2) + Math.Pow(this.Y, 2));
+
+        public bool Equals(Position pos) => 
+            this.X.Equals(pos.X) && this.Y.Equals(pos.Y);
+
+        public Position Clone() => new Position(this.X, this.Y);
+
+        public override string ToString() => 
+            "(" + this.X.ToString() + ", " + this.Y.ToString() + ")";
+
+        public static bool operator > (Position a, Position b) => 
+            !a.Length().Equals(b.Length())
+                ? a.Length() > b.Length()
+                : a.X >= b.X;
+
+        public static bool operator < (Position a, Position b) => 
+            !a.Length().Equals(b.Length())
+                ? a.Length() < b.Length()
+                : a.X <= b.X;
+
+        public static Position operator + (Position a, Position b) =>
+            new Position(a.X + b.X, a.Y + b.Y);
+
+        public static Position operator - (Position a, Position b) =>
+            new Position(Math.Abs(a.X - b.X), Math.Abs(a.Y - b.Y));
+
+        public static double operator % (Position a, Position b) =>
+            Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+    }
+}
---
+++ Coordinate/SortedPosList.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coordinate
+{
+    class SortedPosList
+    {
+        private List<Position> positions;
+
+        public SortedPosList() => this.positions = new List<Position>();
+
+        public int Count() => positions.Count;
+
+        public void Add(Position p)
+        {
+            for (int i = 0; i < Count(); i++)
+            {
+                if (p < positions[i])
+                {
+                    positions.Insert(i, p);
+                    return;
+                }
+            }
+
+            positions.Add(p);
+        }
+
+        public bool Remove(Position pos)
+        {
+            bool removed = false;
+
+            for (int i = 0; i < Count(); i++)
+            {
+                if (positions[i].Equals(pos))
+                {
+                    positions.RemoveAt(i);
+                    removed = true;
+                }
+            }
+
+            return removed;
+        }
+
+        public SortedPosList Clone()
+        {
+            SortedPosList clone = new SortedPosList();
+            foreach (Position pos in positions)
+            {
+                Position cPos = pos.Clone();
+                clone.Add(cPos);
+            }
+            return clone;
+        }
+
+        public SortedPosList CircleContent(Position centerPos, double radius)
+        {
+            SortedPosList insideCircleList = new SortedPosList();
+            foreach (Position pos in positions)
+            {
+                // (Math.Pow(pos.X - centerPos.X, 2) + Math.Pow(pos.Y - centerPos.Y, 2) < Math.Pow(radius, 2))
+                if (pos % centerPos <= radius)
+                    insideCircleList.Add(pos);
+            }
+            return insideCircleList;
+        }
+
+        public static SortedPosList operator + (SortedPosList sp1, SortedPosList sp2)
+        {
+            SortedPosList sum = new SortedPosList();
+            foreach (Position pos in sp1.positions)
+                sum.Add(pos);
+            foreach (Position pos in sp2.positions)
+                sum.Add(pos);
+            return sum;
+        }
+        
+        public override string ToString() => String.Join(", ", positions);            }
+}
